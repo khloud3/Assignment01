38773ed6924f4db839b879cc229deacdb302d066
khloud3 <132269763+khloud3@users.noreply.github.com>
2023-05-08 10:13:34 +0300

    Add files via upload

---
+++ Assighenment1/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+/*:
+## Exercise - Variables
+ 
+ Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
+ */
+var schooling = 17
+print("My total years of school i have completed" , +schooling)
+
+//:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
+schooling = 18
+print ("My total years of school i have completed" , +schooling)
+
+//:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
+print("The frist time in constant we cannot change the value but in variable we can do it")
+
+/*:
+[Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
+ */
---
+++ Assighenment1/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+/*:
+## App Exercise - Fitness Tracker: Constant or Variable?
+ 
+ >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ 
+ There are all sorts of things that a fitness tracking app needs to keep track of in order to display the right information to the user. Similar to the last exercise, declare either a constant or a variable for each of the following items, and assign each a sensible value. Be sure to use proper naming conventions.
+ 
+- Name: The user's name
+- Age: The user's age
+- Number of steps taken today: The number of steps that a user has taken today
+- Goal number of steps: The user's goal for number of steps to take each day
+- Average heart rate: The user's average heart rate over the last 24 hours
+ */
+let userName = "khloud"
+print ("Because each person have one name ")
+var userAge = 25
+print ("Because the age is changed value ")
+var userSteps = 5000
+print ("Because every day you can do it a different result")
+var userGoal = 10000
+print ("Because every day you can set a different goal")
+var userHeartRate = "90 bpm"
+print("Because every day you have a different result")
+
+/*:
+ Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
+ 
+[Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Types and Type Safety](@next)
+ */
